314660e9496bfb38c25ee6c94db098eba3ad6e99
Victor Alves <victor.alvesf7@gmail.com>
2019-03-24 14:10:48 -0400

    Removendo arrays declarados

--- alg.js
+++ alg.js
@@ -2,9 +2,6 @@ const shellsort = require('./shellsort');
 const quickSort = require('./quicksort');
 const random = require('./random');
 
-const array = [ 2000, 2, 10, 1000, 9, 274, 28, 98]
-const arrayQuickSort = [ 5, 1, 4, 2, 3, 10, 9 ];
-
 const iniciarArray = ( array ) => ({
     algoritmoDeOrdenacao: ( algortimo ) => algortimo( array ) 
 });
